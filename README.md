5e2197231bae8a365e775645ba154a522753db2b
rosell-dk <bjorn@rosell.dk>
2022-04-05 11:56:03 +0200

    mostly copied from image-mime-type-guesser

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Image Mime Sniffer
+# Image Mime Type Sniffer
+
+Gets mime type of common **image** files by sniffing the file signature.
+The fact that this library limits its ambition to sniff images makes it quite lite and simple
+
+The library recognizes common image formats, such as GIF, JPEG, WEBP, JPEG-2000 and HEIF
+Here is the full list:
+
+- application/psd
+- image/bmp
+- image/gif
+- image/heic
+- image/heif
+- image/jp2
+- image/jp20
+- image/jpeg
+- image/jpm
+- image/jpx
+- image/png
+- image/svg+xml
+- image/tiff
+- image/webp
+- image/x-icon
+- video/mj2
+
+TODO: AVIF
+
+# Alternatives
+
+There are quite a few PHP mime type detectors out there:
+
+- https://github.com/Intervention/mimesniffer
+- https://github.com/Tinram/File-Identifier
+- https://github.com/shanept/MimeSniffer
+- https://github.com/zjsxwc/mime-type-sniffer
+- https://github.com/thephpleague/mime-type-detection/tree/main/src
---
+++ tests/ImageMimeTypeSnifferTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\ImageMimeTypeSniffer;
+
+use \ImageMimeTypeSniffer\ImageMimeTypeSniffer;
+use \PHPUnit\Framework\TestCase;
+
+class ImageMimeTypeSnifferTest extends TestCase
+{
+
+    private function mimeSniff($fileName)
+    {
+        return ImageMimeTypeSniffer::detect(__DIR__ . '/images/' . $fileName);
+    }
+
+    public function testGIF()
+    {
+        $this->assertEquals('image/gif', $this->mimeSniff('gif-test.gif'));
+    }
+
+    public function testPNG()
+    {
+        $this->assertEquals('image/png', $this->mimeSniff('png-test.png'));
+        $this->assertEquals('image/png', $this->mimeSniff('png-very-small.png'));
+        $this->assertEquals('image/png', $this->mimeSniff('png-with-jpeg-extension.jpg'));
+        $this->assertEquals('image/png', $this->mimeSniff('png-without-extension'));
+        $this->assertEquals('image/png', $this->mimeSniff('png-not-true-color.png'));
+    }
+
+    public function testJpeg()
+    {
+        $this->assertEquals('image/jpeg', $this->mimeSniff('jpg-test.jpg'));
+        $this->assertEquals('image/jpeg', $this->mimeSniff('jpg-ending-with-dot.jpg.'));
+        $this->assertEquals('image/jpeg', $this->mimeSniff('jpg-with space.jpg'));
+        $this->assertEquals('image/jpeg', $this->mimeSniff('.jpg-beginning-with-dot.jpg'));
+    }
+
+    public function testJpeg2000()
+    {
+        $this->assertEquals('image/jp2', $this->mimeSniff('jpeg-2000-jp2-test.jp2'));
+        // TODO: test jpx, etc
+    }
+
+    public function testTiff()
+    {
+        $this->assertEquals('image/tiff', $this->mimeSniff('tif-test.tif'));
+    }
+
+    public function testWebP()
+    {
+        $this->assertEquals('image/webp', $this->mimeSniff('webp-test.webp'));
+    }
+
+    /* --------- below here: files that are not images, should NOT be recognized ------------- */
+    public function testTxt()
+    {
+        $this->assertNull($this->mimeSniff('not-images/txt-test.txt'));
+    }
+    public function testODT()
+    {
+        $this->assertNull($this->mimeSniff('not-images/odt-test.odt'));
+    }
+
+    /*
+    public function testICO()
+    {
+        $this->assertEquals('image/ico', $this->mimeSniff('ico-test.ico'));
+        $this->assertEquals('image/ico', $this->mimeSniff('ico2-test.ico'));
+    }*/
+
+
+//        doDetectTest('not-images/txt-test.txt', false);
+
+}
